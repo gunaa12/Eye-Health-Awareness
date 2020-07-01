489d6dbd6880fde4727734d4b4dacf92b6c48c60
gunaa1 <43862441+gunaa1@users.noreply.github.com>
2020-07-01 17:28:16 -0400

    Add files via upload

---
+++ Eye Health Awareness - Nextech Summer Coding Challenge/js/footer.js
@@ -0,0 +1,57 @@
+document.write(
+    '<footer class="site-footer bg-dark">\
+        <div class="container ">\
+            <div class="row ">\
+                <div class="col-lg-3.5">\
+                    <div class="col-md-12 ">\
+						<h3 class="footer-heading mb-4 ">Creators:</h3>\
+                        <div>\
+                            <ul>\
+                                <li>By: Guna Avula </li>\
+                                <li> With help from: </li>\
+                                <ul>\
+                                    <li><a href = "https://www.w3schools.com/"> W3 Schools </a></li>\
+                                    <li><a href = "https://stackoverflow.com/"> Stack Overflow </a></li>\
+                                </ul>\
+                                <li>For: Nextech Coding Challenge</li>\
+                                <ul>\
+                                    <li><a href = "https://nextech.org/programs/summer-competition/">Nextech - Website</a></li>\
+                                    <li><a href = "https://twitter.com/nextech">Nextech - Twitter</a></li>\
+                                    <li><a href = "mailto:coding@nextech.org">Nextech - Email</a></li>\
+                                <ul>\
+                            </ul>\
+						</div>\
+					</div>\
+				</div>\
+                <div style="margin-left:0.5em;" class="col-lg-4">\
+                    <div class="col-md-12">\
+                    <h3 class="footer-heading mb-4 ">For More Information:</h3>\
+                    <div>\
+                        <ul>\
+                            <li><a href = "https://eye.keckmedicine.org/how-covid-19-affects-the-eyes/">USC Roski Institute</a></li>\
+                            <li><a href = "https://www.nvisioncenters.com/coronavirus-and-the-eyes/">NVision</a></li>\
+                            <li><a href = "https://www.mayoclinic.org/healthy-lifestyle/adult-health/in-depth/office-ergonomics/art-20046169">Mayo Clinic</a></li>\
+                            <li><a href = "https://www.aoa.org/patients-and-public/caring-for-your-vision/low-vision/what-causes-low-vision">American Optometric Association</a></li>\
+                            <li><a href = "http://ergonomictrends.com/proper-sitting-posture-computer-experts/">Ergonomic Trends</a></li>\
+                            <li><a href = "https://www.aoa.org/patients-and-public/caring-for-your-vision/protecting-your-vision/computer-vision-syndrome#:~:text=Computer%20Vision%20Syndrome%2C%20also%20referred,digital%20screens%20for%20extended%20periods.">American Optometric Association</a></li>\
+                        </ul>\
+                    </div>\
+                </div>\
+                </div>\
+				<div class="col-lg-3">\
+                    <div class="col-md-12 ">\
+                        <h3 class="footer-heading mb-4">Navigation:</h3>\
+                    </div>\
+                    <div class="col-md-6 col-lg-6 ">\
+                        <ul class="list-unstyled ">\
+                            <li><a href="index.html">Home</a></li>\
+                            <li><a href="tracker.html">Screen Time</a></li>\
+                            <li><a href="faq.html">FAQ</a></li>\
+                            <li><a href="#">Top of Page</a></li>\
+                        </ul>\
+                    </div>\
+                </div>\
+            </div>\
+        </div>\
+    </footer>'
+);
